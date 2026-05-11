c8607bbfad91ca40b96b2342889477a1f0a674f1
علي ALI <hay476588@gmail.com>
2026-05-11 07:23:26 +0000

    صورة للقنابل https://cdn.discordapp.com/attachments/1502024701365588140/

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
